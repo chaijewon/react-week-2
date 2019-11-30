63d04cf9d38b07b95df6972d92c9d531755e0490
chaijewon <vcandjava@nate.com>
2019-11-30 14:46:11 +0900

    first1

--- movie-server.js
+++ movie-server.js
@@ -20,6 +20,11 @@ const port=3355;
    })
 
  */
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 app.listen(port,()=>{
     console.log("Server Start...","http://localhost:3355")
 })
--- src/MovieMain.js
+++ src/MovieMain.js
@@ -1,12 +1,44 @@
 import React,{Component,Fragment} from 'react'
-
+import axios from 'axios'
+// 외부서버에서 데이터 읽기
 class MovieMain extends Component{
     constructor(props) {
         super(props);
         this.state={
-            title:'박스오피스'
+            title:'박스오피스',
+            movie_data:[],
+            movie_detail:{},
+            open:false
         }
     }
+    // state에 대한 초기값 ==> movie?no=1
+    /*
+           class A
+           {
+               A(){
+                  this => A
+               }
+               B b=new B(){
+                  this => B
+               }
+           }
+           class B
+           {
+           }
+     */
+    componentDidMount() {
+        var _this=this;
+       axios.get('http://localhost:3355/movie',{
+           params:{
+               no:1
+           }
+       }).then(function (response) {
+          console.log(response.data)
+           _this.setState({movie_data:response.data}) // render()
+
+       })
+    }
+
     onBtnClick(no){
         if(no===1){
            this.setState({title:'박스오피스'})
@@ -27,6 +59,18 @@ class MovieMain extends Component{
            "margin":"0px auto",
            "width":"960px"
        }
+       /*
+         "thumbUrl":"/common/mast/movie/2019/11/thumb/thn_5658
+          d961585b46dd8b05225665af2c5a.jpg"
+        */
+       const html=this.state.movie_data.map((m,index)=>
+          <tr key={index}>
+              <td><img src={"http://www.kobis.or.kr/"+m.thumbUrl} width={"35"} height="35"/></td>
+              <td>{m.movieNm}</td>
+              <td>{m.genre}</td>
+              <td>{m.openDt}</td>
+          </tr>
+       )
        return (
            <Fragment>
                <div className={"row"} style={style}>
@@ -37,7 +81,19 @@ class MovieMain extends Component{
 
                   </div>
                    <div className={"col-sm-7"}>
-
+                     <table className={"table table-striped"}>
+                         <thead>
+                           <tr className={"success"}>
+                               <th></th>
+                               <th>영화명</th>
+                               <th>장르</th>
+                               <th>개봉일</th>
+                           </tr>
+                         </thead>
+                         <tbody>
+                         {html}
+                         </tbody>
+                     </table>
                    </div>
                </div>
                <div className={"row"} style={style}>
